5a53312059f4ea81c8939b6b0da083a7eacdfde0
Maglio79 <chris.maglio79@gmail.com>
2016-04-28 18:11:41 -0700

    UGH

---
+++ iJoke/iJoke/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iJoke
+//
+//  Created by Chris Maglio on 4/27/16.
+//  Copyright © 2016 Chris Maglio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
